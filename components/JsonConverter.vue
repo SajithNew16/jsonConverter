b3a8785f4c04ae02e5e46a47bbb3086bab600cd1
SajithL-Aet <sajith.l@aeturnum.com>
2021-11-21 09:11:07 +0530

    project initiation

---
+++ components/JsonConverter.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-row>
+    <v-col cols="6">
+      <v-container class="input-text-area">
+        <v-textarea
+          label="Input Json String"
+          v-model="input"
+          :hint="hint"
+          rows="20"
+        >
+        </v-textarea>
+      </v-container>
+    </v-col>
+    <v-col cols="6">
+      <v-container class="output-text-area">
+        <v-textarea
+          label="Converted Json Object"
+          v-model="output"
+          readonly
+          rows="20"
+        ></v-textarea>
+      </v-container>
+    </v-col>
+  </v-row>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      hint: '"{\\"name\\":\\"John\\"}"', // "{\"name\":\"John\"}"
+      input: "",
+      output: "",
+    };
+  },
+  watch: {
+    input(val) {
+      try {
+        this.output = JSON.parse(val);
+      } catch {
+        this.output = "invalid json string";
+      }
+    },
+  },
+};
+</script>
+<style scoped>
+.input-text-area {
+  border: 4px solid blue;
+}
+.output-text-area {
+  border: 4px solid red;
+}
+</style>
